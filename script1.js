23d7669cc9afd9a08df05bdf14b0ae89b3558bde
Raissa Desyandita Tedjomurti <72061462+raissadesyandita@users.noreply.github.com>
2020-11-03 22:31:32 +0700

    Update
    Success to changing from "Binar, Academy" to "Binar Academy".

--- script1.js
+++ script1.js
@@ -1,36 +1,39 @@
 // =========================================================================
-/* ---Binar, Academy--- */
+/* ---Binar Academy--- */
 
 
 const getFirstName = () => {
-    const promise = new Promise((resolve, reject) => {
-      setTimeout(() => {
-        return (true) ? resolve("Binar") : reject(Error("Gagal"))
-      }, 2000)
-    });
-    return promise;
-  }
-  
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      return (true) ? resolve("Binar") : reject(Error("Gagal"))
+    }, 2000)
+  });
+  return promise;
+}
+
 const getLastName = () => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        return (true) ? resolve(` Academy`) : reject(Error("Gagal"))
-      }, 3000)
-    })
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      return (true) ? resolve(` Academy`) : reject(Error("Gagal"))
+    }, 3000)
+  })
 }
 
 const handleOnClick = () => {
-const hasilEl = document.querySelector("#hasil")
-hasilEl.innerText = "Please wait..."
-    console.log("Clicked...")
-    Promise.all([getFirstName(), getLastName()])
-        .then(response => {
-          document.querySelector("#hasil").innerText = response
-        })
-        .catch(error => {
-            document.querySelector("#hasil").innerText = "Error!"
-            console.log(error)
-        })
+  const hasilEl = document.querySelector("#hasil")
+  hasilEl.innerText = "Please wait..."
+  console.log("Clicked...")
+
+  Promise.all([
+    getFirstName(),
+    getLastName()
+  ])
+  .then(response => {
+    document.querySelector("#hasil").innerText = `${response[0]} ${response[1]}`
+  })
+  .catch(error => {
+    document.querySelector("#hasil").innerText = "Error!"
+  })
 }
 
 
